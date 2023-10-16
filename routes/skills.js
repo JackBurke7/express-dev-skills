0263b6f9194128e141d1c971d970bce7194c455e
jack burke <jeburkemusic@gmail.com>
2023-10-16 11:32:08 -0400

    Part_2

--- routes/skills.js
+++ routes/skills.js
@@ -3,7 +3,28 @@ const router = express.Router();
 const skillsController = require('../controllers/skills');
 
 router.get('/', skillsController.index);
-
 router.get('/:id', skillsController.show);
+router.get('/new', (req, res) => {
+    res.render('skills/new');
+});
+
+router.post('/', (req, res) => {
+    const { name, level, description } = req.body;
+    res.redirect('/skills');
+});
+
+router.delete('/:id', (req, res) => {
+    const skillId = parseInt(req.params.id);
+    res.redirect('/skills');
+});
+
+router.get('/:id/edit', (req, res) => {
+    const skillId = parseInt(req.params.id);
+    res.render('skills/edit', { skill });
+});
+
+router.put('/:id', (req, res) => {
+    const skillId = parseInt(req.params.id);
+});    
 
 module.exports = router;
